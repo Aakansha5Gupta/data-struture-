6e15c2ef8b7acc21182e70d714b7d8a332cc882f
Aakansha5Gupta <aakanshag87@gmail.com>
2026-03-28 22:13:00 +0530

    Add files via upload

---
+++ upper_triangular.c
@@ -0,0 +1,33 @@
+ #include<stdio.h>
+int main()
+{
+    int arr[3][3], i, j;
+
+    printf("Enter 3 X 3 matrix:\n");
+
+    // Input
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+
+    printf("Upper Triangular Matrix:\n");
+
+    // Upper triangular
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            if(j >= i)
+                printf("%d ", arr[i][j]);
+            else
+                printf("0 ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
