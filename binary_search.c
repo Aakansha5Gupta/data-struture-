7ec3519d5f9568b57fb7d48ac19e09bdb6377695
Aakansha5Gupta <aakanshag87@gmail.com>
2026-03-28 13:10:34 +0530

    Add files via upload

---
+++ binary_search.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int arr[10],i,n,search,first,last,middle;
+    printf("enter number of elements:");
+    scanf("%d",&n);
+    printf("enter all %d integers in sorted array\n",n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }    
+    printf("enter value to find :\n");
+    scanf("%d",&search);
+    first=0;
+    last=n-1;
+    middle= (first+last)/2;
+    while (first<=last)
+    {
+        if(arr[middle]<search)
+        first=middle+1;
+        else if (arr[middle]==search)
+        {
+            printf("%d found at location%d\n",search,middle);
+            break;
+        }
+        else 
+        last=middle-1;
+        middle=(first+last)/2;
+    }
+        if (first>last)
+        printf("%d is not found in the array\n",search);
+    return 0 ;
+}
